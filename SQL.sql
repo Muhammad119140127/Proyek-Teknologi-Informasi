724b37fd899c81839f1717dced247c44ff2f755d
wirasenaip <muhammad.119140127@student.itera.ac.id>
2022-05-19 22:01:48 +0700

    SQL

--- SQL.sql
+++ SQL.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 16, 2022 at 07:20 PM
+-- Generation Time: May 19, 2022 at 07:05 AM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -56,7 +56,34 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2019_08_19_000000_create_failed_jobs_table', 1),
-(3, '2022_04_15_203640_create_produk_table', 1);
+(3, '2022_04_15_203640_create_produk_table', 1),
+(4, '2022_05_07_181418_create_pesanans_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesanans`
+--
+
+CREATE TABLE `pesanans` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `idProduk` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `namaPemesan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nomorTelepon` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `size` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `qty` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kodePesanan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `pesanans`
+--
+
+INSERT INTO `pesanans` (`id`, `idProduk`, `namaPemesan`, `nomorTelepon`, `size`, `qty`, `status`, `kodePesanan`, `created_at`, `updated_at`) VALUES
+(1, '39', 'Example User', '08009988775588', 'L 65', '2', 'Dikirim', '1918153090', '2022-05-18 21:52:55', '2022-05-18 21:53:34');
 
 -- --------------------------------------------------------
 
@@ -70,7 +97,6 @@ CREATE TABLE `produk` (
   `model` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `harga` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
-  `diskon` int(11) NOT NULL,
   `files` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -80,8 +106,10 @@ CREATE TABLE `produk` (
 -- Dumping data for table `produk`
 --
 
-INSERT INTO `produk` (`id`, `nama`, `model`, `harga`, `quantity`, `diskon`, `files`, `created_at`, `updated_at`) VALUES
-(5, 'green', 't-shirt', 30000, 3, 3, '1650060041.png', '2022-04-15 15:00:41', '2022-04-15 15:00:41');
+INSERT INTO `produk` (`id`, `nama`, `model`, `harga`, `quantity`, `files`, `created_at`, `updated_at`) VALUES
+(37, 'Shirt', 't-shirt', 1200000, 10, 'img-produk/q7ow7DTUdOiOPPBOUcAbO0VX7FicCChvj2gIFRhD.jpg', '2022-05-18 09:11:17', '2022-05-18 21:49:05'),
+(38, 'Hoodie Abu', 'hoodie', 120000, 12, 'img-produk/APRQcdGCv4IrRc5hDUcodUntljoPHQyIUBZQ5Dlm.png', '2022-05-18 09:14:15', '2022-05-18 21:48:29'),
+(39, 'Blue Shirt', 't-shirt', 120000, 12, 'img-produk/qWNojlMPklkednYhkJs8sW8yESB4gK1voQTHiA9y.jpg', '2022-05-18 21:52:07', '2022-05-18 21:52:07');
 
 -- --------------------------------------------------------
 
@@ -117,6 +145,12 @@ ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `pesanans`
+--
+ALTER TABLE `pesanans`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `produk`
 --
 ALTER TABLE `produk`
@@ -143,13 +177,19 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `pesanans`
+--
+ALTER TABLE `pesanans`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `users`
